eedeb55ac9c2b07c7acf130139f12395f78be720
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-09-28 16:40:18 -0600

    Finalización de la introducción a TanStack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,20 @@
-
-import { useEffect, useState } from 'react'
+import { useRandom } from './hooks/userRandom';
 import './App.css'
-
-// https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new
 
 function App() {
 
-  const [number, setNumber] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState('');
-
-  const [refreshToken, setRefreshToken] = useState(0);
-
-  useEffect(() => {
-    setIsLoading(true);
-
-    fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new')
-      .then(resp => resp.json())
-      .then(data => {
-        setNumber(data);
-      })
-      .catch(error => setError(error))
-      .finally(() => setIsLoading(false));
-
-  }, [refreshToken]);
+  const { randomQuery } = useRandom();
 
   return (
     <>
       {
-        isLoading ? (<h1>Caregando</h1>) : <h1>Número: {number}</h1>
+        randomQuery.isFetching ? (<h1>Cargando</h1>) : <h1>Número: {randomQuery.data}</h1>
       }
 
-      <div>{error}</div>
-      <button disabled={isLoading} onClick={() => setRefreshToken(refreshToken + 1)}>Nuevo número</button>
+      {/* <RandomNumber /> */}
+
+      <div>{JSON.stringify(randomQuery.error)}</div>
+      <button disabled={randomQuery.isFetching} onClick={() => randomQuery.refetch()}>Nuevo número</button>
 
     </>
   )
---
+++ src/hooks/userRandom.tsx
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+
+const getCryptoNumber = async (): Promise<number> => {
+    // throw 'No se pudo obtener el número';
+
+    const resp = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new')
+        .then(resp => resp.json());
+
+    return Number(resp);
+}
+
+export const useRandom = () => {
+    const randomQuery = useQuery({
+        queryKey: ['randomNumber'],
+        queryFn: getCryptoNumber,
+        // retry: false,
+        // retryDelay: 1000,
+        // refetchOnWindowFocus: false,
+        // staleTime: 1000 * 5
+    });
+
+    return {
+        randomQuery
+    };
+}
